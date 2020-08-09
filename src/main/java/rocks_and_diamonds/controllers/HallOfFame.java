cffa170b64a3c35c5856fe89861263727f99d94a
krzjoz <krz_joz@windowslive.com>
2020-08-09 20:18:01 +0200

    Changes in Hall of fame, Options, Quit

--- src/main/java/rocks_and_diamonds/controllers/HallOfFame.java
+++ src/main/java/rocks_and_diamonds/controllers/HallOfFame.java
@@ -1,8 +1,21 @@
 package rocks_and_diamonds.controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import rocks_and_diamonds.GameState;
 
 public class HallOfFame extends StateController{
+	
+	@FXML
+	private StackPane stackPane;
+	@FXML
+	private VBox vBox;
+	@FXML
+	private Label label;
+	
 
 	@Override
 	public void setParent(GameState parent) {
--- src/main/java/rocks_and_diamonds/controllers/Options.java
+++ src/main/java/rocks_and_diamonds/controllers/Options.java
@@ -1,10 +1,22 @@
 package rocks_and_diamonds.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import rocks_and_diamonds.GameState;
 import rocks_and_diamonds.GameStates;
 
@@ -14,21 +26,134 @@ public class Options extends StateController{
 	
 	@FXML
 	private StackPane stackPane;
-	
 	@FXML
-	private Label label;
+	private HBox hBox;	
+	@FXML
+	private VBox vBox1;
+	@FXML
+	private VBox vBox2;
+	@FXML
+	private Button music;
+	@FXML
+	private Button texture;
+	@FXML
+	private Button difficulty;
+	@FXML
+	private Button backToMenu;
+	@FXML
+	private TextField tfmusic;
+	@FXML
+	private TextField tftexture;
+	@FXML
+	private TextField tfdifficulty;
+	
+	private List<Button> buttons;
+	private int btnIndex;
+	private String btnGeneralStyle;
+	private DropShadow btnShadow;
 	
 	
 	@FXML
 	public void initialize() {
-		label.setFocusTraversable(true);
+		
+		buttons = new ArrayList<Button>();
+		buttons.add(music);
+		buttons.add(texture);
+		buttons.add(difficulty);
+		buttons.add(backToMenu);
+		
+		btnIndex = 0;
+		btnGeneralStyle = "-fx-background-color: lightgray;";
+		btnShadow = new DropShadow(BlurType.values()[0], Color.rgb(215, 156, 36), 0, 3.0f, 3.0f, 3.0f);
+		focusOnButton(btnIndex);
+
+		for (Button btn : buttons) {
+			btn.setFocusTraversable(false);
+
+			btn.setCursor(Cursor.HAND);
+		}
+
+		{// KeyListener na menuBox
+			vBox1.setFocusTraversable(true);
+			EventHandler<KeyEvent> keyPressed = new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent e) {
+					buttonOnKeyEvent(e);
+				}
+			};
+			vBox1.addEventHandler(KeyEvent.KEY_PRESSED, keyPressed);
+		} // KeyListener na menuBox
+		
 	}
 	
-	public void labelKeyPressed(KeyEvent e) {
-		System.out.println("OPTIONS");
-		if(e.getCode() == KeyCode.SPACE )
+	
+	public void musicOnMouseClicked() {
+		
+	}
+	
+	public void textureOnMouseClicked() {
+		
+	}
+	
+	public void difficultyOnMouseClicked() {
+		
+	}
+	
+	public void returnOnMouseClicked() {
 			parent.mainWindow().changeState(GameStates.MENU);
 	}
+	
+	public void onEnter(MouseEvent e) {
+		for(int i = 0; i < buttons.size(); i++) {
+			if(e.getTarget().hashCode() == buttons.get(i).hashCode())
+				btnIndex = i;
+		}
+		focusOnButton(btnIndex);
+	}
+	
+	public void buttonOnKeyEvent(KeyEvent e) {
+		if(e.getCode() == KeyCode.DOWN) {
+			if(btnIndex < buttons.size() - 1)
+				btnIndex++;
+			else
+				btnIndex = 0;
+		}else if (e.getCode() == KeyCode.UP){
+			if(btnIndex > 0)
+				btnIndex--;
+			else
+				btnIndex = buttons.size() - 1;
+		}else if (e.getCode() == KeyCode.ENTER) {
+			String id = buttons.get(btnIndex).getId();
+			if (id.equals("music")) {
+				
+			} else if (id.equals("texture")) {
+				
+			}
+			else if (id.equals("difficulty")) {
+			
+			}
+			else if (id.equals("backToMenu")) {
+				parent.mainWindow().changeState(GameStates.MENU);
+			}
+				
+		}
+		
+		focusOnButton(btnIndex);		
+	}
+	
+	private void focusOnButton(int btnIndex) {
+		for(int i = 0; i < buttons.size(); i++) {
+			Button btn = buttons.get(i);
+			if(i == btnIndex) {
+				btn.setStyle(btnGeneralStyle+"-fx-text-fill: black;");
+				btn.setEffect(btnShadow);
+			}else {
+				btn.setStyle(btnGeneralStyle+"-fx-text-fill: grey;");
+				btn.setEffect(null);
+			}
+		}
+	}
+
 	
 	@Override
 	public void setParent(GameState parent) {
