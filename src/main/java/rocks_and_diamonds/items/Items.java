246f87a16c8c1abe9fd79ad4f23f0421834579c5
Łukasz Paszko <lpaszko30@gmail.com>
2020-08-09 22:31:44 +0200

    duzo zmian

--- src/main/java/rocks_and_diamonds/items/Items.java
+++ src/main/java/rocks_and_diamonds/items/Items.java
@@ -7,10 +7,10 @@ public enum Items {
 	VOID(0, 0, 0),
 	DIRT(130, 65, 0),
 	WALL(150, 150, 150),
-	RED(255,0,0),
-	GREEN(0,255,0),
-	BLUE(0,0,255),
-	YELLOW(255,255,0);
+	RED_DIAMOND(255,0,0),
+	GREEN_DIAMOND(0,255,0),
+	BLUE_DIAMOND(0,0,255),
+	YELLOW_DIAMOND(255,255,0);
 	
 	private Color color;
 	
